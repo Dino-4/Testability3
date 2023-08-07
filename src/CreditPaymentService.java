b2b322db6abb3cc70809e83d1a9e1ce8d75427d9
DiNo-4 <120940819+Dino-4@users.noreply.github.com>
2023-08-07 14:07:44 +0400

    origin

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,9 @@
 public class CreditPaymentService {
-    public int payment (int sum, double percent, double term){
-    double mps = percent / 12 /100;
-    double cps = mps * Math.pow((1 + mps), term) / (Math.pow((1+mps), term) - 1);
-    cps = cps * sum;
-    return (int) cps;
+    public int payment(int sum, double percent, double term) {
+        double mps = percent / 12 / 100;
+        double cps = mps * Math.pow((1 + mps), term) / (Math.pow((1 + mps), term) - 1);
+        cps = cps * sum;
+        return (int) cps;
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-    CreditPaymentService calculate = new CreditPaymentService();
-    int result = calculate.payment(1_000_000,9.99, 36);
+        CreditPaymentService calculate = new CreditPaymentService();
+        int result = calculate.payment(1_000_000, 9.99, 36);
         System.out.println("Ежемесячный платеж " + result + " ₽");
     }
 }
